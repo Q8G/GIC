4f9e1ab6a1cbea3bdf80b61096a6aba30054a2dc
L <74691792+Q8G@users.noreply.github.com>
2025-06-11 13:13:59 +0300

    GIC

--- GIC/main.py
+++ GIC/main.py
@@ -2,6 +2,6 @@ try:from github import Github
 except ImportError:import os;os.system(f"pip install PyGithub")
 from github import Github
 Token="ghp_UR_Token"
-repo="GIC"
+repository="GIC"
 path="main.py"
-while True:git=Github(Token);repo=git.get_repo(f"{git.get_user().login}/{repo}");file=repo.get_contents(path);repo.update_file(file.path,repo.get_commits(path=path).get_page(0)[0].commit.message,f'{file.decoded_content.decode("utf-8")}',file.sha)
+while True:git=Github(Token);repo=git.get_repo(f"{git.get_user().login}/{repository}");file=repo.get_contents(path);repo.update_file(file.path,repo.get_commits(path=path).get_page(0)[0].commit.message,f'{file.decoded_content.decode("utf-8")}',file.sha)
